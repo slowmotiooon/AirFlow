4abc15280343b2a7c800b27622e79127dd84062c
dynandu <dynandu2022@outlook.com>
2024-01-28 23:56:54 +0800

    新增了识别命令的代码，可以成功从命令中分出命令类型和命令值。

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,40 @@ void loop() {
   //在其它蓝牙设备向esp32输入蓝牙信息后，esp32会把接收到的信息放入缓冲区，
   //当缓冲区的字符长度不为0时，avaliable为true，否则为false。
   while(SerialBT.available()){
-    Serial.write(SerialBT.read());  //读取SerialBT获取到的内容，并在串口监视器中输出
+    cmdBuffer +=char(SerialBT.read());  //读取SerialBT中的内容，并且存放到cmdBuffer中，便于后续操作。
 
   }
-
+  //Serial的available函数也同理。
   while(Serial.available()){
     SerialBT.write(Serial.read());
+  }
+
+  if(cmdBuffer.length()){
+    String commandType; //存储的命令类型
+
+    //遍历cmdBuffer，分割命令类型和命令参数；
+    for(int i = 0;i<cmdBuffer.length();i++){
+      if(isalpha(cmdBuffer[i])){
+        commandType+=cmdBuffer[i];
+      }
+      if(isdigit(cmdBuffer[i])){
+        cmdval = cmdval*10+cmdBuffer[i]-'0';
+      }
+    }
+
+    Serial.print("Command: ");
+    Serial.print(commandType);
+    Serial.print(" Value: ");
+    Serial.print(cmdval, DEC);
+    Serial.print('\n');
+
+
+
+    // LED响应
+    // Serial.print(cmdBuffer);
+    digitalWrite(LED_BUILTIN,HIGH);
+    delay(10);
+    digitalWrite(LED_BUILTIN,LOW);
   }
 }
 
